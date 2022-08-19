56ee1c4cc0567af3769a963b514eb085a24e5554
4med <105792721+4med@users.noreply.github.com>
2022-08-19 16:01:07 +0300

    Update MainActivity.java

--- app/src/main/java/com/better/amino/activites/MainActivity.java
+++ app/src/main/java/com/better/amino/activites/MainActivity.java
@@ -19,8 +19,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Account account = new Account(this);
-
-
+        
         login_btn = findViewById(R.id.login_btn);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
