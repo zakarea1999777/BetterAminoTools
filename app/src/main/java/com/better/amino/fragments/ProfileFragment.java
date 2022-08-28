ab248845941093c1864e1414684c35c2a0d2fa55
4med <105792721+4med@users.noreply.github.com>
2022-08-28 14:22:40 +0300

    Clean Code

--- app/src/main/java/com/better/amino/fragments/ProfileFragment.java
+++ app/src/main/java/com/better/amino/fragments/ProfileFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.better.amino.R;
@@ -30,15 +33,6 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     CircleImageView icon;
     TextInputEditText nickname;
     TextInputEditText bio;
@@ -46,38 +40,6 @@ public class ProfileFragment extends Fragment {
     CircleImageView camera;
     File image = null;
     String filepath;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -89,14 +51,14 @@ public class ProfileFragment extends Fragment {
         camera = view.findViewById(R.id.camera_icon);
 
         RequestOptions options = new RequestOptions().centerCrop().placeholder(R.mipmap.ic_launcher_round).error(R.mipmap.ic_launcher_round);
-        
+
         String url = "";
 
         try {url = AccountUtils.icon.replace("http", "https");}
         catch (Exception ignored) {}
-         
+
         Glide.with(requireContext()).load(Uri.parse(url)).apply(options).into(icon);
-    
+
         nickname.setText(AccountUtils.nickname);
         bio.setText(AccountUtils.bio);
         set.setProgress(0);
@@ -108,31 +70,29 @@ public class ProfileFragment extends Fragment {
         });
 
         icon.setOnClickListener(vie -> {
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType("image/*");
-            startActivityForResult(intent, 200);
+            profileFragmentResultLauncher.launch(new Intent(Intent.ACTION_PICK).setType("image/*"), ActivityOptionsCompat.makeBasic());
         });
 
         return view;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
-            if (data == null) {return;}
+    ActivityResultLauncher<Intent> profileFragmentResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
 
-            try {
-                InputStream inputStream = requireContext().getContentResolver().openInputStream(data.getData());
-                icon.setImageBitmap(BitmapFactory.decodeStream(inputStream));
-                inputStream.close();
+                try {
+                    InputStream inputStream = requireContext().getContentResolver().openInputStream(data.getData());
+                    icon.setImageBitmap(BitmapFactory.decodeStream(inputStream));
+                    inputStream.close();
 
-                String filePath = FileUtils.GetRealPathFromUri(data.getData(), requireActivity());
-                this.filepath = filePath;
-                image = new File(filePath);
+                    String filePath = FileUtils.GetRealPathFromUri(data.getData(), requireActivity());
+                    this.filepath = filePath;
+                    image = new File(filePath);
+                }
+                catch (IOException ignored) {}
             }
-            catch (IOException ignored) {}
-        }
-    }
+        });
 }
