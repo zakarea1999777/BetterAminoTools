b5c04eecd766cd9b356e5605bb16375e51e8d405
4med <105792721+4med@users.noreply.github.com>
2022-08-31 17:45:31 +0300

    Delete app/src/main/java/com/better/amino directory

--- app/src/main/java/com/better/amino/api/utils/CommunityUtils.java
+++
@@ -1,8 +0,0 @@
-package com.better.amino.api.utils;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class CommunityUtils {
-    public static Map<String, Object> community = new HashMap<>();
-}
