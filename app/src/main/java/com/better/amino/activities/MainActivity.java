03438dcbc5fc7f3ccfd1081b2c550a1aadb3f1d0
4med <105792721+4med@users.noreply.github.com>
2022-08-31 16:40:04 +0300

    Add files via upload

--- app/src/main/java/com/better/amino/activities/MainActivity.java
+++ app/src/main/java/com/better/amino/activities/MainActivity.java
@@ -3,19 +3,11 @@ package com.better.amino.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.better.amino.R;
 import com.better.amino.api.Account;
 import com.better.amino.api.utils.AccountUtils;
 import com.better.amino.ui.SharedValue;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
